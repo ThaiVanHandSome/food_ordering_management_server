5cdb12a30d8b6ea6790f4cd62ab128a38dd4aff1
vulevan2206 <148562104+vulevan2206@users.noreply.github.com>
2024-10-12 15:19:34 +0700

    Update category.services.ts

--- src/services/category.services.ts
+++ src/services/category.services.ts
@@ -1,44 +1,36 @@
 import { CategoryModel } from '../models/category.model';
+import { ErrorHandler } from '../utils/response';
+import { STATUS } from '~/constants/httpStatus';
 
-// Hàm lấy tất cả danh mục
 export const getAllCategories = async () => {
   try {
     const categories = await CategoryModel.find();
-    return categories;
+    return { message: 'Lấy tất cả danh mục thành công', data: categories };
   } catch (error) {
-    throw error;
+    throw error
   }
 };
 
-// Hàm lấy danh mục theo ID
 export const getCategoryById = async (categoryId: string) => {
-  try {
-    const category = await CategoryModel.findById(categoryId);
-    return category;
-  } catch (error) {
-    throw error;
+  const category = await CategoryModel.findById(categoryId);
+  if (!category) {
+    throw new ErrorHandler(STATUS.NOT_FOUND, 'Danh mục không tồn tại');
   }
+  return { message: 'Lấy danh mục thành công', data: category };
 };
 
-// Hàm thêm danh mục mới
 export const addCategory = async (name: string) => {
-  try {
-    const newCategory = new CategoryModel({ name });
-    const savedCategory = await newCategory.save();
-    return savedCategory;
-  } catch (error) {
-    throw error;
-  }
+  const newCategory = new CategoryModel({ name });
+  const savedCategory = await newCategory.save();
+  return { message: 'Tạo danh mục thành công', data: savedCategory };
 };
 
-// Hàm cập nhật danh mục theo ID
 export const updateCategory = async (categoryId: string, name: string) => {
-  try {
-    const updatedCategory = await CategoryModel.findByIdAndUpdate(categoryId, { name }, { new: true });
-    return updatedCategory;
-  } catch (error) {
-    throw error;
+  const updatedCategory = await CategoryModel.findByIdAndUpdate(categoryId, { name }, { new: true });
+  if (!updatedCategory) {
+    throw new ErrorHandler(STATUS.NOT_FOUND, 'Danh mục không tồn tại');
   }
+  return { message: 'Chỉnh sửa danh mục thành công', data: updatedCategory };
 };
 
 export default { getAllCategories, getCategoryById, addCategory, updateCategory };
