2979577b1bc3459bdfb6c2e6ccebd154e47cb564
Khoagec <violympichongkhoa@gmail.com>
2024-10-24 16:28:27 +0700

    CRUD user
    CRUD user nhưng thiếu upload image do t không biết dùng sao cả

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser } from '~/controllers/user.controller'
+import { createUser, deleteUser, getUsers, updateUser, updateUserById } from '~/controllers/user.controller'
 import authMiddleware from '~/middlewares/auth.middleware'
 import { upload, uploadImageToCloudinary } from '~/middlewares/upload.middleware'
 import { wrapAsync } from '~/utils/response'
@@ -12,5 +12,11 @@ router.post(
   uploadImageToCloudinary,
   wrapAsync(createUser)
 )
+
+router.get('/', wrapAsync(getUsers));
+router.post('/', wrapAsync(createUser));
+router.put('/add', wrapAsync(updateUser));
+router.put('/:id', wrapAsync(updateUserById));
+router.delete('/:id', wrapAsync(deleteUser));
 
 export default router
